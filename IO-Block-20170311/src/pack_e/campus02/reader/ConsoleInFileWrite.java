ef2235bd8760d3ea659375f07228f4aae093c1be
Stephan Kochauf <stephan.kochauf@lv.campus02.at>
2017-03-11 15:57:42 +0100

    final

--- IO-Block-20170311/src/pack_e/campus02/reader/ConsoleInFileWrite.java
+++ IO-Block-20170311/src/pack_e/campus02/reader/ConsoleInFileWrite.java
@@ -1,42 +1,39 @@
 package pack_e.campus02.reader;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 
-public class ConsoleReaderApp {
-
+public class ConsoleInFileWrite {
 	public static void main(String[] args) {
-		BufferedReader br = null;
 		
-		try
-		{
-			br = new BufferedReader(
+		try (BufferedReader br = new BufferedReader(
 					new InputStreamReader(System.in)
 					);
+				
+				FileWriter pw = new FileWriter(
+						"D:\\PR2\\noten.txt");)
+		{
+			
 			
 			String line;
 			while ((line = br.readLine()) != null)
 			{
-				System.out.println(line);
+				if (!line.equals("STOP"))
+				{
+					pw.write(line + "\r\n");			
+				}
+				else 
+					break;
 			}
-		}catch (IOException e) {
+			pw.flush();
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		finally
-		{
-			try {
-				br.close();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
 
 	}
-
 }
